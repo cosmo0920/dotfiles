743db357a1ff271aa1a385192cf0da0c968383fe
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-04-19 00:55:36 +0900

    add windows language setting and modify my-langenv.el

---
+++ emacs/emacs.d/mylib/lang-env-win32.el
@@ -0,0 +1,22 @@
+(set-language-environment "Japanese")
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'japanese-cp932) ;; Shift-JIS
+
+;;; IME の設定
+(setq default-input-method "W32-IME")
+(setq-default w32-ime-mode-line-state-indicator "[--]") ;; おこのみで
+(setq w32-ime-mode-line-state-indicator-list '("[--]" "[あ]" "[--]")) ;; おこのみで
+(w32-ime-initialize)
+  
+;;; IME ON/OFF 時にカーソル色を変える。
+(setq ime-activate-cursor-color "#00a000")
+(setq ime-inactivate-cursor-color "#000000")
+(set-cursor-color ime-inactivate-cursor-color)
+;; ※input-method-activate-hook, input-method-inactivate-hook じゃない方がいい感じになる。
+(add-hook 'w32-ime-on-hook
+	  (function (lambda ()
+		      (set-cursor-color ime-activate-cursor-color))))
+(add-hook 'w32-ime-off-hook
+	  (function (lambda ()
+		      (set-cursor-color ime-inactivate-cursor-color))))
+(provide 'lang-env-w32)
