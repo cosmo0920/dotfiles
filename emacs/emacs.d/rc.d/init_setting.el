a4f754cae2a2e72af1916a49eebf55e2e922ab4f
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-08-02 22:27:04 +0900

    add recursive grep

--- emacs/emacs.d/rc.d/init_setting.el
+++ emacs/emacs.d/rc.d/init_setting.el
@@ -65,4 +65,22 @@
 (setq kill-whole-line t)
 ;;to ensure that your files have no trailing whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; 再帰的にgrep
+;; http://www.clear-code.com/blog/2012/3/20.html
+(require 'grep)
+(setq grep-command-before-query "grep -nH -r -e ")
+(defun grep-default-command ()
+  (if current-prefix-arg
+      (let ((grep-command-before-target
+             (concat grep-command-before-query
+                     (shell-quote-argument (grep-tag-default)))))
+        (cons (if buffer-file-name
+                  (concat grep-command-before-target
+                          " *."
+                          (file-name-extension buffer-file-name))
+                (concat grep-command-before-target " ."))
+              (+ (length grep-command-before-target) 1)))
+    (car grep-command)))
+(setq grep-command (cons (concat grep-command-before-query " .")
+                         (+ (length grep-command-before-query) 1)))
 (provide 'init_setting)
