5b3bed9d69f1e1ba4849e31daf7b0f5724a2d4a7
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-08-17 15:45:40 +0900

    add setting deplicate line

--- emacs/emacs.d/mylib/my-func.el
+++ emacs/emacs.d/mylib/my-func.el
@@ -30,6 +30,20 @@
     (set-window-buffer (selected-window) thisbuf)))
 (global-set-key [f2] 'swap-screen)
 (global-set-key [S-f2] 'swap-screen-with-cursor)
+
+(defun duplicate-line (&optional numlines)
+  "One line is duplicated wherever there is a cursor."
+  (interactive "p")
+  (let* ((col (current-column))
+         (bol (progn (beginning-of-line) (point)))
+         (eol (progn (end-of-line) (point)))
+         (line (buffer-substring bol eol)))
+    (while (> numlines 0)
+      (insert "\n" line)
+      (setq numlines (- numlines 1)))
+    (move-to-column col)))
+(define-key esc-map "k" 'duplicate-line)
+
 ;; Prefixのghciとシステムのghciを切り替える
 (when (locate-library "haskell-mode")
   (global-set-key "\C-\M-h"
