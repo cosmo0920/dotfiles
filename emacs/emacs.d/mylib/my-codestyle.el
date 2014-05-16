83774d6e2ed2667e0699d120551c86019b7ba038
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-05-16 10:19:00 +0900

    add Java coding style

--- emacs/emacs.d/mylib/my-codestyle.el
+++ emacs/emacs.d/mylib/my-codestyle.el
@@ -226,3 +226,8 @@
              (c-set-offset 'arglist-close 0)
              )
           )
+;; Java mode hook
+(add-hook 'java-mode-hook (lambda ()
+                            (setq c-basic-offset 4
+                                  tab-width 4
+                                  indent-tabs-mode t)))
