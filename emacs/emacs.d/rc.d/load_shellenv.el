471fe2a43d06775817888b09e0532673cb3a9efb
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-01-12 21:20:24 +0900

    Add guard for Win32 environment
    In Win32 environment, it does not use PATH for now.

--- emacs/emacs.d/rc.d/load_shellenv.el
+++ emacs/emacs.d/rc.d/load_shellenv.el
@@ -1,5 +1,6 @@
 ;; load environment value
-(load-file (expand-file-name "~/.emacs.d/shellenv.el"))
-(dolist (path (reverse (split-string (getenv "PATH") ":")))
-  (add-to-list 'exec-path path))
+(unless run-w32
+      (load-file (expand-file-name "~/.emacs.d/shellenv.el"))
+      (dolist (path (reverse (split-string (getenv "PATH") ":")))
+	(add-to-list 'exec-path path)))
 (provide 'load_shellenv)
