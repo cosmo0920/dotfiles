97552f1bbf14295601dc930013eb72fc85c63e88
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-01-24 15:36:22 +0900

    Brush up NTEmacs setting

--- emacs/emacs.d/mylib/lang-env-win32.el
+++ emacs/emacs.d/mylib/lang-env-win32.el
@@ -1,11 +1,11 @@
-(set-language-environment "Japanese")
 (set-default-coding-systems 'utf-8)
 (set-keyboard-coding-system 'japanese-cp932) ;; Shift-JIS
-
+(set-language-info "Japanese" 'input-method "W32-IME")
+(set-language-environment "Japanese")
+(setq w32-ime-buffer-switch-p nil)
 ;;; IME の設定
-(setq default-input-method "W32-IME")
-(setq-default w32-ime-mode-line-state-indicator "[--]") ;; おこのみで
-(setq w32-ime-mode-line-state-indicator-list '("[--]" "[あ]" "[--]")) ;; おこのみで
+(setq-default w32-ime-mode-line-state-indicator "[--]")
+(setq w32-ime-mode-line-state-indicator-list '("[--]" "[あ]" "[--]"))
 ;; (w32-ime-initialize)
 
 ;;; IME ON/OFF 時にカーソル色を変える。
@@ -14,9 +14,9 @@
 (set-cursor-color ime-inactivate-cursor-color)
 ;; ※input-method-activate-hook, input-method-inactivate-hook じゃない方がいい感じになる。
 (add-hook 'w32-ime-on-hook
-	  (function (lambda ()
-		      (set-cursor-color ime-activate-cursor-color))))
+          (function (lambda ()
+                      (set-cursor-color ime-activate-cursor-color))))
 (add-hook 'w32-ime-off-hook
-	  (function (lambda ()
-		      (set-cursor-color ime-inactivate-cursor-color))))
+          (function (lambda ()
+                      (set-cursor-color ime-inactivate-cursor-color))))
 (provide 'lang-env-win32)
