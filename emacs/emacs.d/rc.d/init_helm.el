e1b7f4ad23de1985abb0278fdd77392c0141673a
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2018-04-08 13:29:52 +0900

    Integrate rencentf sources into helm-mini

--- emacs/emacs.d/rc.d/init_helm.el
+++ emacs/emacs.d/rc.d/init_helm.el
@@ -2,10 +2,18 @@
   (require 'helm)
 
   (require 'helm-config)
+  (require 'helm-source)
   (helm-mode 1)
+
+  (custom-set-variables
+   '(helm-mini-default-sources '(helm-source-buffers-list
+                                 helm-source-recentf
+                                 helm-source-files-in-current-dir
+                                 )))
   (define-key global-map (kbd "C-x C-f") 'helm-find-files)
   (define-key global-map (kbd "C-x C-r") 'helm-recentf)
-  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+  (define-key global-map (kbd "C-x C-b") 'helm-buffers-list)
+  (define-key global-map (kbd "C-x b") 'helm-mini)
   (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 )
 (provide 'init_helm)
